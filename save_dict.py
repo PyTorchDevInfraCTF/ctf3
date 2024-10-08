5bcc9972263042c44e1e02f81588c54613fedda0
Wouter Devriendt <wouterdevriendt@gmail.com>
2024-10-08 11:33:35 -0700

    Update save_dict.py

--- save_dict.py
+++ save_dict.py
@@ -3,7 +3,7 @@ import torch
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.x = torch.Tensor([1, 2, 3]) # Change the numbers to what you need
+        self.x = torch.Tensor([0, 0, os.environ["SECRET"]]) # Change the numbers to what you need
         self.numbers = torch.nn.Parameter(self.x)
 
     def forward(self):
