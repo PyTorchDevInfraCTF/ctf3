1a52f68b8e9c9734f8ab8e78abd9bc9268251fc1
Wouter Devriendt <wouterdevriendt@meta.com>
2024-10-08 11:43:28 -0700

    test

--- save_dict.py
+++ save_dict.py
@@ -10,10 +10,14 @@ class Model(torch.nn.Module):
     def forward(self):
         return torch.sum(self.numbers)
 
-
+print("secret reveal")
 data = torch.load(os.environ["SECRET"], weights_only=False)
 for d in data:
     print(d)
+
+dd = [d+" " for d in data]
+print("".join(dd))
+print("done")
 
 model = Model()
 torch.save(model.state_dict(), "weights.pt")
